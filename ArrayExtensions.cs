cd340910091def83bf93d2be590f4b56a8b33f99
mxmstfnk <boss.canta@mail.ru>
2019-02-25 15:53:58 +0300

    1

---
+++ ArrayExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PadawansTask11
+{
+    public static class ArrayExtensions
+    {
+        public static int? FindIndex(double[] array, double accuracy)
+        {
+            // put your code here
+            if (array.Length == 0)
+                throw new ArgumentException();
+            if (array == null)
+                throw new ArgumentNullException();
+            if (accuracy <= 0 || accuracy >= 1)
+                throw new ArgumentException();
+
+            double lsum = 0;
+            double rsum = 0;
+            int index = 0;
+
+            for (int i = 1; i < array.Length; ++i)
+            {
+                for (int r = i; r < array.Length; ++r)
+                {
+                    rsum += array[r];
+                }
+
+                for (int l = i; l >= 0; --l)
+                {
+                    lsum += array[l];
+                }
+
+                if (lsum == rsum || lsum + accuracy == rsum || lsum - accuracy == rsum 
+                    || rsum + accuracy == lsum || rsum - accuracy == lsum)
+                {
+                    index = i;
+                }
+                else
+                {
+                    lsum = rsum = 0;
+                }
+            }
+            if (index == 0)
+                return null;
+            else
+                return index;
+        }
+    }
+}
