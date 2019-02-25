53371f520f3148d51ba8a0b7f11f14cbbcffd27c
mxmstfnk <boss.canta@mail.ru>
2019-02-25 16:23:07 +0300

    2

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -9,41 +9,43 @@ namespace PadawansTask11
             // put your code here
             if (array.Length == 0)
                 throw new ArgumentException();
-            if (array == null)
+            else if (array == null)
                 throw new ArgumentNullException();
-            if (accuracy <= 0 || accuracy >= 1)
-                throw new ArgumentException();
-
-            double lsum = 0;
-            double rsum = 0;
-            int index = 0;
-
-            for (int i = 1; i < array.Length; ++i)
-            {
-                for (int r = i; r < array.Length; ++r)
-                {
-                    rsum += array[r];
-                }
-
-                for (int l = i; l >= 0; --l)
-                {
-                    lsum += array[l];
-                }
-
-                if (lsum == rsum || lsum + accuracy == rsum || lsum - accuracy == rsum 
-                    || rsum + accuracy == lsum || rsum - accuracy == lsum)
-                {
-                    index = i;
-                }
-                else
-                {
-                    lsum = rsum = 0;
-                }
-            }
-            if (index == 0)
-                return null;
+            else if (accuracy <= 0 || accuracy >= 1)
+                throw new ArgumentOutOfRangeException();
             else
-                return index;
+            {
+                double lsum = 0;
+                double rsum = 0;
+                int index = 0;
+
+                for (int i = 1; i < array.Length; ++i)
+                {
+                    for (int r = i; r < array.Length; ++r)
+                    {
+                        rsum += array[r];
+                    }
+
+                    for (int l = i; l >= 0; --l)
+                    {
+                        lsum += array[l];
+                    }
+
+                    if (lsum == rsum || lsum + accuracy == rsum || lsum - accuracy == rsum
+                        || rsum + accuracy == lsum || rsum - accuracy == lsum)
+                    {
+                        index = i;
+                    }
+                    else
+                    {
+                        lsum = rsum = 0;
+                    }
+                }
+                if (index == 0)
+                    return null;
+                else
+                    return index;
+            }
         }
     }
 }
